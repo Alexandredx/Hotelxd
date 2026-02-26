9a074b6cb9b5381f9b188b9e74dec6b975380b6f
Alexandredx <miloisaac2014@gmail.com>
2026-02-26 11:45:08 -0500

    rutas xd

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsuarioController;
+use App\Http\Controllers\ReservaController;
+use App\Http\Controllers\PagoController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Rutas para los usuarios
+Route::resource('usuarios', UsuarioController::class);
+
+// Rutas para las reservas
+Route::resource('reservas', ReservaController::class);
+
+// Rutas para los pagos
+Route::get('pagos/create/{reservaId}', [PagoController::class, 'create'])->name('pagos.create');
+Route::post('pagos/store/{reservaId}', [PagoController::class, 'store'])->name('pagos.store');
